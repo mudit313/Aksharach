8cbcb8a4e8e02f70440fd1c024868d548e2a797d
muddu <muditag313@gmail.com>
2016-02-27 17:16:51 +0530

    placing done

--- Scrabble/Assets/Scripts/place.cs
+++ Scrabble/Assets/Scripts/place.cs
@@ -3,45 +3,56 @@ using System.Collections;
 
 public class place : MonoBehaviour {
 	public GameObject board;
-	private bool onboard=false;
-	Vector3 bottom;
-	void OnTriggerEnter2D(Collider2D other)
+	//private bool onboard=false;
+	//Vector3 bottom;
+	/*void OnTriggerEnter2D(Collider2D other)
 	{
 		Debug.Log ("Hey");
 		if (other.gameObject.tag == "board") {
 			onboard=true;
 		}
-	}
+	}*/
 	// Update is called once per frame
-	void Update () {
+	/*void Update () {
 		if (onboard == true) {
 			bottom = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0);
+			Debug.Log("Bot");
 			onboard = false;
 		}
-	}
+	}*/
 	void OnMouseUp()
 	{
 		Debug.Log ("hi");
-		Vector3 boardpos = board.transform.position;
-		float sizeBoard = boardpos.y-bottom.y;
+		Vector3 boardpos = new Vector3(board.transform.position.x,board.transform.position.y,0);
+		float sizeBoard = (float)(boardpos.y+4.21);
 		float sizeTile = sizeBoard/16;
 		boardpos.x += sizeTile;
 		boardpos.y += sizeTile;
-		Vector3 offset = new Vector3 (Mathf.Abs(Input.mousePosition.x - boardpos.x),Mathf.Abs(Input.mousePosition.y - boardpos.y), 0);
-		while (offset.x>sizeTile && offset.y>sizeTile) 
-		{
-			if(Input.mousePosition.x>boardpos.x)
-				boardpos.x+=2*sizeTile;
-			else
-				boardpos.x-=2*sizeTile;
-			if(Input.mousePosition.y>boardpos.y)
-				boardpos.y+=2*sizeTile;
-			else
-				boardpos.y-=2*sizeTile;
-			offset.x-=2*sizeTile;
-			offset.y-=2*sizeTile;
+		Vector3 offset = new Vector3 (Mathf.Abs(transform.position.x - boardpos.x),Mathf.Abs(transform.position.y - boardpos.y), 0);
+		Debug.Log (transform.position.y);
+		while (offset.x>sizeTile) {
+			if (transform.position.x > boardpos.x) {
+				boardpos.x += 2 * sizeTile;
+				offset.x -= 2 * sizeTile;
+			} else {
+				boardpos.x -= 2 * sizeTile;
+				offset.x -= 2 * sizeTile;
+			}
 		}
-		Debug.Log ("holla");
+		while (offset.y>sizeTile) {
+			if(transform.position.y>boardpos.y)
+			{
+				boardpos.y+=2*sizeTile;
+				offset.y-=2*sizeTile;
+				continue;
+			}
+			else
+			{
+				boardpos.y-=2*sizeTile;
+				offset.y-=2*sizeTile;
+				continue;
+			}
+		}		
 		transform.position = boardpos;
 	}
 }
