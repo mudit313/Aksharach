7c04c78fbedb0fe2ac29a8eb8738b70ae9d45b57
China <chinangshuk@iitg.ernet.in>
2016-04-12 19:56:32 +0530

    uni

--- Scrabble/Assets/Scripts/drag.cs
+++ Scrabble/Assets/Scripts/drag.cs
@@ -8,12 +8,19 @@ public class drag : MonoBehaviour {
 	private Vector3 offset;
 	void OnMouseDown() 
 	{
-		if(gameObject.GetComponent<place>().placed==false)
+		if(gameObject.GetComponent<place>().placed==false && gameObject.tag!="mixed")
 			offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
 		if (gameObject.GetComponent<place>().onboard == true) {
 			Vector2 ind=Chance.getIndex(gameObject);
-			Board.matrix[(int)ind.x,(int)ind.y]=0;
-			Chance.letteronboard = gameObject;
+			if(gameObject.tag!="mixed")
+			{
+				Board.matrix[(int)ind.x,(int)ind.y]-=Chance.letteronboard.GetComponentInChildren<Point>().pt;
+				Chance.letteronboard = gameObject;
+			}
+			else
+			{
+				gameObject.GetComponent<intersection>().
+			}
 		}
 
 	}
